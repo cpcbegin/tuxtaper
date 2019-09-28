5c85abc4d6b5b9443c3ca33e678918ecf6b4fc67
Jesús B.L <teamcpc@gmail.com>
2019-09-28 23:50:04 +0200

    Conectores botones. Selector de archivo al hacer clic en stop. Inclusión de imagen de cassette vacío.

--- tuxtaper.py
+++ tuxtaper.py
@@ -1,4 +1,4 @@
-import sys, re, signal
+import sys, re
 from tuxtaperui import *
 
 
@@ -7,6 +7,33 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.ui = Ui_FormMain()
         self.ui.setupUi(self)
+
+        self.ui.pushButtonRecord.clicked.connect(self.press_record)
+        self.ui.pushButtonPlay.clicked.connect(self.press_play)
+        self.ui.pushButtonRewind.clicked.connect(self.press_rewind)
+        self.ui.pushButtonAdvance.clicked.connect(self.press_advance)
+        self.ui.pushButtonStop.clicked.connect(self.press_stop)
+        self.ui.pushButtonPause.clicked.connect(self.press_pause)
+
+    def press_record(self):
+        print("Record")
+
+    def press_play(self):
+        print("Play")
+
+    def press_rewind(self):
+        print("Rewind")
+
+    def press_advance(self):
+        print("Advance")
+
+    def press_stop(self):
+        filename, filter = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", ".")
+        self.ui.labelFilename.setText(filename)
+        print("Stop")
+
+    def press_pause(self):
+        print("Pause")
 
 
 if __name__ == "__main__":
